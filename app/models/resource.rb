80d1024177d816508390ac156248d8681807fdb5
Jacinda <jacindaz@gmail.com>
2017-02-06 08:58:44 -0800

    Add additional validations to resource model

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,3 +1,7 @@
 class Resource < ApplicationRecord
   validates :language, inclusion: { in: ["ruby", "python", "sql", "javascript", "java", "c", "clojure", "haskell", "elixir"] }
+  validates :title, presence: true
+  validates :url, presence: true
+  validates :description, presence: true
+  validates :free, inclusion: {in: [true, false]}
 end
