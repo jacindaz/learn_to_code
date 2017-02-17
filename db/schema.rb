201c8f16f1512bfceaaf7b4fe0d68900bb3766ca
Jacinda <jacindaz@gmail.com>
2017-02-17 12:09:29 -0800

    Files generated by monban generator, plus migrating
     - ran bin/rails g monban:scaffold
     - ran bin/rails db:migrate
     - these are the files that resulted

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170206223615) do
+ActiveRecord::Schema.define(version: 20170217200718) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,14 @@ ActiveRecord::Schema.define(version: 20170206223615) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.index ["language"], name: "index_resources_on_language", using: :btree
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",           null: false
+    t.string   "password_digest", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
   end
 
 end
