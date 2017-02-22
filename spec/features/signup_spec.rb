d48d6cc832d5ef0bacaff35c0d8662531cf5fc4c
Jacinda <jacindaz@gmail.com>
2017-02-22 10:25:46 -0500

    Signup integration spec to test monban

---
+++ spec/features/signup_spec.rb
@@ -0,0 +1,19 @@
+require "rails_helper"
+
+feature 'Sign up' do
+  scenario 'user can sign up' do
+    visit root_path
+    click_link "Sign up"
+    expect(current_path).to eq(new_user_path)
+
+    email = "testemail2@email.com"
+    password = "test_password2"
+
+    fill_in "Email", with: email
+    fill_in "Password", with: password
+    click_button "Sign up"
+
+    expect(page).to have_content(email)
+    expect(current_path).to eq(root_path)
+  end
+end
