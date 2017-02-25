67dd3c359b87edf85bae2400076ff554b322d5ff
Jacinda <jacindaz@gmail.com>
2017-02-24 22:57:35 -0500

    Add additional sign up specs for testing monban

--- spec/features/signup_spec.rb
+++ spec/features/signup_spec.rb
@@ -19,3 +19,37 @@ feature "Sign up" do
     expect(current_path).to eq(root_path)
   end
 end
+
+feature "Signing in" do
+  given(:email) { "testemail2@email.com" }
+  given(:password) { "test_password2" }
+
+  background do
+    Monban.config.sign_up_service.new(email: email, password: password).perform
+  end
+
+  scenario "user can sign in" do
+    visit root_path
+    click_link "Sign in"
+    expect(current_path).to eq(new_session_path)
+
+    fill_in "Email", with: email
+    fill_in "Password", with: password
+    click_button "Sign in"
+
+    expect(page).to have_content(email)
+    expect(current_path).to eq(root_path)
+  end
+
+  scenario "user should receive error when wrong password" do
+    visit root_path
+    click_link "Sign in"
+    expect(current_path).to eq(new_session_path)
+
+    fill_in "Email", with: email
+    fill_in "Password", with: "wrong_password"
+    click_button "Sign in"
+
+    expect(page).to have_content("incorrect")
+  end
+end
