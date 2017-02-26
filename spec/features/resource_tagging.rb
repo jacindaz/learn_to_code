33ef68a5ba638df92af5ee0ce2da29c3d8d2e0f9
Jacinda <jacindaz@gmail.com>
2017-02-25 21:58:38 -0500

    Resource tagging integration test

---
+++ spec/features/resource_tagging.rb
@@ -0,0 +1,26 @@
+require "rails_helper"
+
+feature "User views resource tags" do
+  scenario "user can view tags" do
+    resource = create(:resource)
+    resource.tag_list.add("tag1", "tag2")
+    resource.save!
+
+    visit resource_path(resource)
+
+    expect(page).to have_content "tag1"
+    expect(page).to have_content "tag2"
+  end
+end
+
+feature "User adds tag to resource" do
+
+  scenario "user can add tags" do
+  end
+
+  scenario "user can remove tags" do
+  end
+
+  scenario "tag added from a different resource can be re-used" do
+  end
+end
