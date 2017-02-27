10ff550d6a710b5c42b277cc72136b3a854524d0
Jacinda <jacindaz@gmail.com>
2017-02-27 14:33:44 -0800

    Feature test to make sure tags show up on resources#index and #show

--- spec/features/resource_tagging_spec.rb
+++ spec/features/resource_tagging_spec.rb
@@ -1,12 +1,23 @@
 require "rails_helper"
 
 feature "User views resource tags" do
-  scenario "user can view tags" do
+  scenario "user can view tags on resources#show" do
     resource = create(:resource)
     resource.tag_list.add("tag1", "tag2")
     resource.save!
 
     visit resource_path(resource)
+
+    expect(page).to have_content "tag1"
+    expect(page).to have_content "tag2"
+  end
+
+  scenario "user can view tags on resources#index" do
+    resource = create(:resource)
+    resource.tag_list.add("tag1", "tag2")
+    resource.save!
+
+    visit resources_path
 
     expect(page).to have_content "tag1"
     expect(page).to have_content "tag2"
